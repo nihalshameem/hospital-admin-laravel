b8330b2dfdd2d267b8b0c7cbb7a09942ec83300e
nihalshameem <nihalshameem6@gmail.com>
2022-04-09 01:43:03 +0530

    edd count balance

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\MotherMedical;
+use App\Models\Patient;
+use Carbon\Carbon;
+use DB;
+
 class HomeController extends Controller
 {
     /**
@@ -22,9 +27,16 @@ class HomeController extends Controller
     public function dashboard_1()
     {
         $page_title = 'Dashboard';
-        $page_description = 'Some description for the page';
+        $page_description = 'Welcome to Thai Care Kallai - District Dashboard';
         $action = __FUNCTION__;
+        $start = Carbon::now();
+        $end = $start->addDays(7);
 
-        return view('home', compact('page_title', 'page_description', 'action'));
+        $active_an_mother = Patient::count();
+        $an_high_risk = DB::table('patients as p')->join('mother_visits as v', 'p.id', '=', 'v.patient_id')->groupBy('p.id')->count();
+        // $edd = DB::table('patients as p')->join('mother_medicals as m', 'p.id', '=', 'm.patient_id')->whereBetween('DATE_FORMAT(m.edd_date, "%Y-%m-%d")', [$start->format('Y-m-d'), $end->format('Y-m-d')])->groupBy('p.id')->count();
+        $edd = MotherMedical::whereBetween('edd_date', [$start->format('Y-m-d'), $end->format('Y-m-d')])->where('edd_date', '<=', $end->format('Y-m-d'))->count();
+
+        return view('home', compact('page_title', 'page_description', 'action', 'active_an_mother', 'an_high_risk', 'edd'));
     }
 }
